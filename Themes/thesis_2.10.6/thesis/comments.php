0f571576777288a8210013b227ccae64a8111769
ariesrei <arsrymeralles@gmail.com>
2021-10-26 06:49:13 +0800

    delete themes

--- Themes/thesis_2.10.6/thesis/comments.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Copyright 2012 DIYthemes, LLC. Patent pending. All rights reserved.
-License: DIYthemes Software License Agreement
-License URI: https://diythemes.com/thesis/rtfm/software-license-agreement/
-
-NOTE: This file only exists to satisfy a WordPress validation check.
-*/
